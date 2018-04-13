de083c432525dbf4a0389917501f825572038643
im-pratham <prathameshmane1234@gmail.com>
2018-04-13 15:48:49 +0530

    Network Player Movement synchronized

---
+++ Assets/ARCameraPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ARCameraPosition : MonoBehaviour {
+
+	public static GameObject cam;
+	public Text debugText;
+	// Use this for initialization
+	void Start () {
+
+		cam = GameObject.Find("ARCamera");
+		Debug.Log( cam.transform.position.ToString() );
+		// you can simply use transform.position if the script is attached to the ARCamera
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	int i = 0;
+	void OnGUI(){
+
+		if( GUI.Button(new Rect(20, 20, 100, 200),"ARCamera Position") ){
+			Debug.Log( cam.transform.position.ToString() );
+			debugText.text = i++ + " " + cam.transform.position.ToString () + " " + cam.transform.rotation;
+		}
+	}
+}
