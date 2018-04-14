c12e58a355a2e557115b95fcf9f1c61735b3ec4e
KushalChandak <kpchandak500@gmail.com>
2018-04-14 22:33:45 +0530

    Updated Scripts

---
+++ Assets/kushal/ScoreMaintain2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreMaintain {
+	public Text score;
+	public static int count = 0;
+	// Use this for initialization
+	void Start () {
+		score.text = "Score :- " + count.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score.text = count + "";
+	}
+	public void SetCount (){
+		//score.text = "Score :- " + counts.ToString ();
+		count++;
+	}
+}
